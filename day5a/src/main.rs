e92fccdc8b43055883e9d557681777c4326c278f
Balazs Marjan <marjanbalazs@outlook.com>
2020-06-25 21:41:33 +0200

    Nice

--- day5a/src/main.rs
+++ day5a/src/main.rs
@@ -6,8 +6,53 @@ going back and forward....
 1, Read the whole file into a vector
 2, Step around in the vector reading opcodes
 */
+
+/*
+First, you'll need to add two new instructions:
+
+    Opcode 3 takes a single integer as input and saves it to the position given by its only parameter. For example, the instruction 3,50 would take an input value and store it at address 50.
+    Opcode 4 outputs the value of its only parameter. For example, the instruction 4,50 would output the value at address 50.
+
+Programs that use these instructions will come with documentation that explains what should be connected to the input and output. The program 3,0,4,0,99 outputs whatever it gets as input, then halts.
+
+Second, you'll need to add support for parameter modes:
+
+Each parameter of an instruction is handled based on its parameter mode. Right now, your ship computer already understands parameter mode 0, position mode, which causes the parameter to be interpreted as a position - if the parameter is 50, its value is the value stored at address 50 in memory. Until now, all parameters have been in position mode.
+
+Now, your ship computer will also need to handle parameters in mode 1, immediate mode. In immediate mode, a parameter is interpreted as a value - if the parameter is 50, its value is simply 50.
+*/
+
 use std::env;
 use std::fs;
+
+enum Op {
+    Add {
+        lhs: OpMode,
+        rhs: OpMode,
+        out: OpMode,
+    },
+    Multiply {
+        lhs: OpMode,
+        rhs: OpMode,
+        out: OpMode,
+    },
+    Store {
+        input: OpMode,
+    },
+    Load {
+        input: OpMode,
+    },
+}
+
+enum OpMode {
+    Position,
+    Immediate,
+}
+
+struct Command {
+    op: Op,
+    args: Vec<i32>,
+}
 
 fn op_add(pos: usize, vec: &mut Vec<i32>) {
     let lhs = vec[pos + 1];
@@ -35,7 +80,7 @@ fn op_load(pos: usize, vec: &mut Vec<i32>) -> i32 {
     return vec[index as usize];
 }
 
-fn process_ops(input: i32, vec: &mut Vec<i32>) -> i32 {
+fn process(input: i32, vec: &mut Vec<i32>) -> i32 {
     let mut x: usize = 0;
     let mut output = 0;
     while x < vec.len() {
@@ -53,9 +98,36 @@ fn process_ops(input: i32, vec: &mut Vec<i32>) -> i32 {
     output
 }
 
+
+fn execute_opcode(op: Command) {
+
+}
+
+
+fn decode_opcode(opcode: i32) -> Command {
+    let mut cmd: Command = match opcode % 100 {
+        0 => {
+            unimplemented!();
+        }
+        1 => {
+            unimplemented!();
+        }
+        2 => {
+            unimplemented!();
+        }
+        99 => {
+            unimplemented!();
+        }
+        _ => {
+            unimplemented!();
+        }
+    };
+    return cmd;
+}
+
 fn parse_file_to_vec(file_path: String) -> Vec<i32> {
     let content = fs::read_to_string(file_path).unwrap();
-    let tokens: Vec<&str> = content.split(",").collect();
+    let tokens: Vec<&str> = content.split(',').collect();
 
     let v: Vec<i32> = tokens
         .into_iter()
@@ -83,7 +155,7 @@ fn main() -> Result<(), ()> {
 
     let mut vec = parse_file_to_vec(file_path.to_string());
 
-    let output = process_ops(input, &mut vec);
+    let output = process(input, &mut vec);
 
     println!("{}", output);
 
