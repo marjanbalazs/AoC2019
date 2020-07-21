bbfaef3ab795de4e30cce2dcc84ae0eada9025cc
marjanbalazs <marjanbalazs@outlook.com>
2020-07-22 00:59:10 +0200

    IntComputer reworked. Day5b success.

--- day5b/src/main.rs
+++ day5b/src/main.rs
@@ -1,11 +1,16 @@
 use std::env;
 use std::fs;
 
+#[derive(Debug)]
 enum Op {
     Add,
     Multiply,
     Store,
     Load,
+    JmpTrue,
+    JmpFalse,
+    Less,
+    Equal,
     Halt,
 }
 
@@ -14,73 +19,200 @@ enum ArgMode {
     Position,
     Immediate,
 }
-
+#[derive(Debug)]
 struct Command {
     op: Op,
     args: Option<Vec<ArgMode>>,
 }
 
-fn op_add(pos: usize, args: Vec<ArgMode>, vec: &mut Vec<i32>) {
-    let lhs = match args[0] {
-        ArgMode::Position => vec[vec[pos + 1 as usize] as usize],
-        ArgMode::Immediate => vec[pos + 1],
-    };
-
-    let rhs = match args[1] {
-        ArgMode::Position => vec[vec[pos + 2 as usize] as usize],
-        ArgMode::Immediate => vec[pos + 2],
-    };
-
-    let res = lhs + rhs;
-
-    match args[2] {
-        ArgMode::Position => {
-            let idx = vec[pos + 3 as usize];
-            vec[idx as usize] = res;
-        }
-        ArgMode::Immediate => vec[pos + 3] = res,
-    };
+struct Machine<'a> {
+    memory: &'a mut Vec<i32>,
+    ip: usize,
+    input: i32,
+    output: i32,
 }
 
-fn op_multiply(pos: usize, args: Vec<ArgMode>, vec: &mut Vec<i32>) {
-    let lhs = match args[0] {
-        ArgMode::Position => vec[vec[pos + 1 as usize] as usize],
-        ArgMode::Immediate => vec[pos + 1],
-    };
-
-    let rhs = match args[1] {
-        ArgMode::Position => vec[vec[pos + 2 as usize] as usize],
-        ArgMode::Immediate => vec[pos + 2],
-    };
-
-    let res = lhs * rhs;
-
-    match args[2] {
-        ArgMode::Position => {
-            let idx = vec[pos + 3 as usize];
-            vec[idx as usize] = res;
-        }
-        ArgMode::Immediate => vec[pos + 3] = res,
-    };
-}
-
-fn op_store(pos: usize, input: i32, args: Vec<ArgMode>, vec: &mut Vec<i32>) {
-    match args[0] {
-        ArgMode::Position => {
-            let index = vec[pos + 1] as usize;
-            vec[index as usize] = input;
-        }
-        ArgMode::Immediate => {
-            let index = pos + 1;
-            vec[index as usize] = input;
+impl<'a> Machine<'a> {
+    fn run(&mut self) {
+        while self.ip < self.memory.len() {
+            println!("Instruction Pointer: {}", self.ip);
+            let op = decode(self.memory[self.ip]);
+            let arg_len = match op.args {
+                Some(x) => x.len(),
+                None => 0,
+            };
+            let args = decode_argmodes(self.memory[self.ip], arg_len);
+            match op.op {
+                Op::Add => self.op_add(args),
+                Op::Multiply => self.op_multiply(args),
+                Op::Store => self.op_store(args),
+                Op::Load => self.op_load(args),
+                Op::JmpTrue => self.op_jmptrue(args),
+                Op::JmpFalse => self.op_jmpfalse(args),
+                Op::Less => self.op_less(args),
+                Op::Equal => self.op_equal(args),
+                Op::Halt => break,
+            }
         }
     }
-}
 
-fn op_load(pos: usize, args: Vec<ArgMode>, vec: &mut Vec<i32>) -> i32 {
-    match args[0] {
-        ArgMode::Position => vec[vec[pos + 1] as usize],
-        ArgMode::Immediate => vec[pos + 1],
+    fn op_add(&mut self, args: Vec<ArgMode>) {
+        let lhs = match args[0] {
+            ArgMode::Position => self.memory[self.memory[self.ip + 1 as usize] as usize],
+            ArgMode::Immediate => self.memory[self.ip + 1],
+        };
+
+        let rhs = match args[1] {
+            ArgMode::Position => self.memory[self.memory[self.ip + 2 as usize] as usize],
+            ArgMode::Immediate => self.memory[self.ip + 2],
+        };
+
+        let res = lhs + rhs;
+
+        match args[2] {
+            ArgMode::Position => {
+                let idx = self.memory[self.ip + 3 as usize];
+                self.memory[idx as usize] = res;
+            }
+            ArgMode::Immediate => self.memory[self.ip + 3] = res,
+        };
+        self.ip += args.len() + 1;
+    }
+
+    fn op_multiply(&mut self, args: Vec<ArgMode>) {
+        let lhs = match args[0] {
+            ArgMode::Position => self.memory[self.memory[self.ip + 1 as usize] as usize],
+            ArgMode::Immediate => self.memory[self.ip + 1],
+        };
+
+        let rhs = match args[1] {
+            ArgMode::Position => self.memory[self.memory[self.ip + 2 as usize] as usize],
+            ArgMode::Immediate => self.memory[self.ip + 2],
+        };
+
+        let res = lhs * rhs;
+
+        match args[2] {
+            ArgMode::Position => {
+                let idx = self.memory[self.ip + 3 as usize];
+                self.memory[idx as usize] = res;
+            }
+            ArgMode::Immediate => self.memory[self.ip + 3] = res,
+        };
+        self.ip += args.len() + 1;
+    }
+
+    fn op_store(&mut self, args: Vec<ArgMode>) {
+        match args[0] {
+            ArgMode::Position => {
+                let index = self.memory[self.ip + 1] as usize;
+                self.memory[index as usize] = self.input;
+                println!("Stored: {}", self.memory[index as usize]);
+            }
+            ArgMode::Immediate => {
+                let index = self.ip + 1;
+                self.memory[index as usize] = self.input;
+                println!("Stored: {}", self.memory[index as usize]);
+            }
+        }
+        self.ip += args.len() + 1;
+    }
+
+    fn op_load(&mut self, args: Vec<ArgMode>) {
+        self.output = match args[0] {
+            ArgMode::Position => self.memory[self.memory[self.ip + 1] as usize],
+            ArgMode::Immediate => self.memory[self.ip + 1],
+        };
+        self.ip += args.len() + 1;
+    }
+
+    fn op_jmptrue(&mut self, args: Vec<ArgMode>) {
+        let cond = match args[0] {
+            ArgMode::Position => self.memory[self.memory[self.ip + 1 as usize] as usize],
+            ArgMode::Immediate => self.memory[self.ip + 1 as usize],
+        };
+
+        let jmp = match args[1] {
+            ArgMode::Position => self.memory[self.memory[self.ip + 2 as usize] as usize],
+            ArgMode::Immediate => self.memory[self.ip + 2 as usize],
+        };
+
+        if cond != 0 {
+            self.ip = jmp as usize;
+        } else {
+            self.ip += args.len() + 1;
+        }
+    }
+
+    fn op_jmpfalse(&mut self, args: Vec<ArgMode>) {
+        let cond = match args[0] {
+            ArgMode::Position => self.memory[self.memory[self.ip + 1 as usize] as usize],
+            ArgMode::Immediate => self.memory[self.ip + 1 as usize],
+        };
+
+        let jmp = match args[1] {
+            ArgMode::Position => self.memory[self.memory[self.ip + 2 as usize] as usize],
+            ArgMode::Immediate => self.memory[self.ip + 2 as usize],
+        };
+
+        if cond == 0 {
+            self.ip = jmp as usize;
+        } else {
+            self.ip += args.len() + 1;
+        }
+    }
+
+    fn op_less(&mut self, args: Vec<ArgMode>) {
+        let lhs = match args[0] {
+            ArgMode::Position => self.memory[self.memory[self.ip + 1 as usize] as usize],
+            ArgMode::Immediate => self.memory[self.ip + 1 as usize],
+        };
+
+        let rhs = match args[1] {
+            ArgMode::Position => self.memory[self.memory[self.ip + 2 as usize] as usize],
+            ArgMode::Immediate => self.memory[self.ip + 2 as usize],
+        };
+
+        let store = if lhs < rhs { 1 } else { 0 };
+
+        match args[2] {
+            ArgMode::Position => {
+                let index = self.memory[self.ip + 3] as usize;
+                self.memory[index as usize] = store;
+            }
+            ArgMode::Immediate => {
+                let index = self.ip + 3;
+                self.memory[index as usize] = store;
+            }
+        }
+
+        self.ip += args.len() + 1;
+    }
+
+    fn op_equal(&mut self, args: Vec<ArgMode>) {
+        let lhs = match args[0] {
+            ArgMode::Position => self.memory[self.memory[self.ip + 1 as usize] as usize],
+            ArgMode::Immediate => self.memory[self.ip + 1 as usize],
+        };
+
+        let rhs = match args[1] {
+            ArgMode::Position => self.memory[self.memory[self.ip + 2 as usize] as usize],
+            ArgMode::Immediate => self.memory[self.ip + 2 as usize],
+        };
+
+        let store = if lhs == rhs { 1 } else { 0 };
+
+        match args[2] {
+            ArgMode::Position => {
+                let index = self.memory[self.ip + 3] as usize;
+                self.memory[index as usize] = store;
+            }
+            ArgMode::Immediate => {
+                let index = self.ip + 3;
+                self.memory[index as usize] = store;
+            }
+        }
+        self.ip += args.len() + 1;
     }
 }
 
@@ -99,6 +231,7 @@ fn decode_argmodes(opcode: i32, len: usize) -> Vec<ArgMode> {
 }
 
 fn decode(opcode: i32) -> Command {
+    println!("Opcode: {:?}", opcode);
     let op: Command = match opcode % 100 {
         1 => {
             let arg_modes = decode_argmodes(opcode, 3);
@@ -128,6 +261,34 @@ fn decode(opcode: i32) -> Command {
                 args: Some(arg_modes),
             }
         }
+        5 => {
+            let arg_modes = decode_argmodes(opcode, 2);
+            Command {
+                op: Op::JmpTrue,
+                args: Some(arg_modes),
+            }
+        }
+        6 => {
+            let arg_modes = decode_argmodes(opcode, 2);
+            Command {
+                op: Op::JmpFalse,
+                args: Some(arg_modes),
+            }
+        }
+        7 => {
+            let arg_modes = decode_argmodes(opcode, 3);
+            Command {
+                op: Op::Less,
+                args: Some(arg_modes),
+            }
+        }
+        8 => {
+            let arg_modes = decode_argmodes(opcode, 3);
+            Command {
+                op: Op::Equal,
+                args: Some(arg_modes),
+            }
+        }
         99 => Command {
             op: Op::Halt,
             args: None,
@@ -136,38 +297,8 @@ fn decode(opcode: i32) -> Command {
             panic!("Unexpected instruction {:?}", x);
         }
     };
+    println!("{:?}", op);
     op
-}
-
-fn process(input: i32, ops: &mut Vec<i32>) -> i32 {
-    let mut output = 0;
-    let mut i = 0;
-    while i < ops.len() {
-        let op = decode(ops[i]);
-        let arg_len = match op.args {
-            Some(x) => x.len(),
-            None => 0,
-        };
-        let argmodes = decode_argmodes(ops[i], arg_len);
-        match op.op {
-            Op::Add => {
-                op_add(i, argmodes, ops);
-            }
-            Op::Multiply => {
-                op_multiply(i, argmodes, ops);
-            }
-            Op::Store => {
-                op_store(i, input, argmodes, ops);
-            }
-            Op::Load => output = op_load(i, argmodes, ops),
-            Op::Halt => {
-                break;
-            }
-        }
-
-        i += arg_len + 1;
-    }
-    output
 }
 
 fn parse_file_to_vec(file_path: String) -> Vec<i32> {
@@ -185,7 +316,7 @@ fn parse_file_to_vec(file_path: String) -> Vec<i32> {
 fn main() -> Result<(), ()> {
     let vec_str: Vec<String> = env::args().collect();
 
-    let (file_path, input) = match vec_str.len() {
+    let (file_path, input_val) = match vec_str.len() {
         0..=2 => {
             panic!("Not enough args");
         }
@@ -198,11 +329,18 @@ fn main() -> Result<(), ()> {
         }
     };
 
-    let mut vec = parse_file_to_vec(file_path.to_string());
+    let mut mem = parse_file_to_vec(file_path.to_string());
 
-    let output = process(input, &mut vec);
+    let mut machine = Machine {
+        memory: &mut mem,
+        ip: 0,
+        input: input_val,
+        output: 0,
+    };
 
-    println!("{}", output);
+    machine.run();
+
+    println!("{}", machine.output);
 
     Ok(())
 }
