142b42b3787b6b220d433554741c62e503d11aae
marjanbalazs <marjanbalazs@outlook.com>
2020-08-26 00:11:30 +0200

    Threadpool setup for the task.

--- day7a/src/main.rs
+++ day7a/src/main.rs
@@ -1,8 +1,67 @@
 // Another Intcode Machine problem
 
+// So this problem screams for a threadpool and a central thread that will string together the inputs and outputs between the stages.
+// The threadpool will run intmachines and the central thread will submit jobs with a "phase" and "input" and expect an output.
+
 use intcode::Machine;
 use std::env;
 use std::fs;
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
+use std::time::Duration;
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+impl ThreadPool {
+    fn new(size: usize) -> ThreadPool {
+        let (sender, receiver) = mpsc::channel();
+        let mut workers = Vec::with_capacity(size);
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        for i in 0..size {
+            workers.push(Worker::new(i, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    // This is hand tailored, but I want to the function to take three arguments... The input, the phase setting and the memory it will operate on
+    fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    handle: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<std::sync::Mutex<std::sync::mpsc::Receiver<Job>>>) -> Worker {
+        let handle = thread::spawn(move|| {
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+
+                println!("Worker {} got a job; executing.", id);
+    
+                job();
+            }
+        });
+        Worker { id, handle }
+    }
+}
 
 fn parse_file_to_vec(file_path: String) -> Vec<i32> {
     let content = fs::read_to_string(file_path).unwrap();
@@ -32,19 +91,27 @@ fn main() -> Result<(), ()> {
         }
     };
 
-    let mut mem = parse_file_to_vec(file_path.to_string());
+    let mem_original = parse_file_to_vec(file_path.to_string());
 
-    let mut machine = intcode::Machine {
-        memory: &mut mem,
-        ip: 0,
-        input: input_val,
-        output: 0,
-    };
-
-
+    /*
     machine.run();
-
     println!("{}", machine.get_output());
+    */
+
+    let threadpool = ThreadPool::new(4);
+
+
+
+    threadpool.execute(move|| {
+        let mut machine = intcode::Machine {
+            memory: &mut mem_original.clone(),
+            ip: 0,
+            input: input_val,
+            output: 0,
+        };
+        machine.run();
+        println!("Result: {}", machine.get_output());
+    });
 
     Ok(())
 }
