ea0d467dea06a572f5f2d1fa7e29062d7658c347
marjanbalazs <marjanbalazs@outlook.com>
2020-09-16 22:58:17 +0200

    Day7a working

--- day7a/src/main.rs
+++ day7a/src/main.rs
@@ -3,75 +3,25 @@
 // So this problem screams for a threadpool and a central thread that will string together the inputs and outputs between the stages.
 // The threadpool will run intmachines and the central thread will submit jobs with a "phase" and "input" and expect an output.
 
-use intcode::Machine;
 use std::env;
 use std::fs;
-use std::sync::mpsc;
-use std::sync::Arc;
-use std::sync::Mutex;
-use std::thread;
-use std::time::Duration;
 
-type Job = Box<dyn FnOnce() + Send + 'static>;
-
-struct ThreadPool {
-    workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
-}
-
-impl ThreadPool {
-    fn new(size: usize) -> ThreadPool {
-        let (sender, receiver) = mpsc::channel();
-        let mut workers = Vec::with_capacity(size);
-
-        let receiver = Arc::new(Mutex::new(receiver));
-
-        for i in 0..size {
-            workers.push(Worker::new(i, Arc::clone(&receiver)));
-        }
-
-        ThreadPool { workers, sender }
+// Generate permutations
+fn gen_permuations(inputs: Vec<i32>, res: &mut Vec<i32>, output: &mut Vec<Vec<i32>>) {
+    if inputs.len() == 0 {
+        output.push(res.to_vec());
+        return;
     }
-
-    // This is hand tailored, but I want to the function to take three arguments... The input, the phase setting and the memory it will operate on
-    fn execute<F>(&self, f: F)
-    where
-        F: FnOnce() + Send + 'static,
-    {
-        let job = Box::new(f);
-        self.sender.send(job).unwrap();
+    for elem in &inputs {
+        let new_input = inputs.iter().filter(|x| *x != elem).cloned().collect();
+        let mut new_res = res.clone();
+        new_res.push(*elem);
+        gen_permuations(new_input, &mut new_res, output);
     }
 }
 
-impl Drop for ThreadPool {
-    fn drop(&mut self) {
-        for worker in &mut self.workers {
-            println!("Shutting down worker {}", worker.id);
-            if let Some(thread) = worker.handle.take() {
-                thread.join().unwrap();
-            }
-        }
-    }
-}
-
-struct Worker {
-    id: usize,
-    handle: Option<thread::JoinHandle<()>>,
-}
-
-impl Worker {
-    fn new(id: usize, receiver: Arc<std::sync::Mutex<std::sync::mpsc::Receiver<Job>>>) -> Worker {
-        let handle = thread::spawn(move|| {
-            loop {
-                let job = receiver.lock().unwrap().recv().unwrap();
-
-                println!("Worker {} got a job; executing.", id);
-    
-                job();
-            }
-        });
-        Worker { id, handle: Some(handle) }
-    }
+fn generate_permutation_iterative() {
+    unimplemented!();
 }
 
 fn parse_file_to_vec(file_path: String) -> Vec<i32> {
@@ -104,25 +54,30 @@ fn main() -> Result<(), ()> {
 
     let mem_original = parse_file_to_vec(file_path.to_string());
 
-    let mut machine = intcode::Machine {
-        memory: &mut mem_original.clone(),
-        ip: 0,
-        input: input_val,
-        output: 0,
-    };
-   
-    let threadpool = ThreadPool::new(4);
+    let mut phase_permutations: Vec<Vec<i32>> = Vec::new();
+    let phase_settings = Vec::from([0, 1, 2, 3, 4]);
 
-    threadpool.execute(move|| {
-        let mut machine = intcode::Machine {
-            memory: &mut mem_original.clone(),
-            ip: 0,
-            input: input_val,
-            output: 0,
-        };
-        machine.run();
-        println!("Result: {}", machine.get_output());
-    });
+    gen_permuations(phase_settings, &mut Vec::new(), &mut phase_permutations);
+
+    let results: Vec<i32> = phase_permutations
+        .into_iter()
+        .map(|phase_settings| {
+            let mut x = 0;
+            for elem in phase_settings {
+                let mut machine = intcode::Machine {
+                    memory: &mut mem_original.clone(),
+                    ip: 0,
+                    input: Vec::from([x, elem]),
+                    output: 0,
+                };
+                machine.run();
+                x = machine.output;
+            }
+            x
+        })
+        .collect();
+
+    println!("{:?}", results.iter().max().unwrap());
 
     Ok(())
 }
